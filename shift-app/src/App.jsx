0d459002e4e86f0fab676087f2565244cf7a5542
fwppx786-design <fwppx786@gmail.com>
2026-05-31 19:58:23 +0900

    Update App.jsx

--- shift-app/src/App.jsx
+++ shift-app/src/App.jsx
@@ -333,9 +333,10 @@ export default function App() {
               {/* 背景画像 */}
             <img src='/kusa.png' style={{
   position: 'absolute',
-  top: '50%', left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: '70%',
+  top: 0, left: 0,
+  width: '100%',
+  height: '100%',
+  objectFit: 'contain',
   opacity: 0.15,
   pointerEvents: 'none',
   zIndex: 0,
